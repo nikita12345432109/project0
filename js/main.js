d9ac935da8bb585d798ac237796c67ae91672f21
nikita12345432109 <167443311+nikita12345432109@users.noreply.github.com>
2024-04-23 20:57:04 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// const swiperTop = new Swiper('.top__swiper', {
+
+//     effect : 'fade',
+
+
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     }
+// });
+
+
+// const swiperAbout = new Swiper(".about__slider", {
+//   slidesPerView: 4,
+//   spaceBetween: 20,
+//   freeMode: true,
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   }
+// });
+
+
+
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
